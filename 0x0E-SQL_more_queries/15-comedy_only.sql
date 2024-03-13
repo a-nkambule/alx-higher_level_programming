6c90719ee8a2c418fd8b75dc75802a52d9c5f3b3
Aviwe <aviwenkambule@gmail.com>
2024-03-13 04:06:16 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
--- lists all Comedy shows in the database htbn_0d_tcshows
--- lists all rows of a database correspomding to a column value
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- lists all rows of a database corresponding to a column value
 SELECT title
 FROM tv_shows
 LET JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
