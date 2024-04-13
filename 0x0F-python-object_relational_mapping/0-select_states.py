a5ec8f7d21aecb87e681f39d94888fa87f80cb5a
Aviwe <aviwenkambule@gmail.com>
2024-04-13 06:22:57 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,27 +15,27 @@ def list_states (username, password, database):
             port=3306,\
             user=username,\
             passwd=password,\
-            db=database)
+            db=database)    
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch all states
+    #Execute the SQL query to fetch all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows from the query result
+    #Fetch all the rows from the query result
     rows = cursor.fetchall()
 
-    # Print the results
+    #Print the results
     for row in rows:
         print(row)
 
-    # Close the database connection
+    #Close the database connection
     db.close()
 
-# Example usage
-if __name__ == '__main__':
+#Example usage
+if__name__ =='__main__':
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+username = sys.argv[1]
+password = sys.argv[2]
+stabase = sys.argv[3]
 
-    list_states(username, password, database)
+list_states(username, password, database)
